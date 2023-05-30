19ca22a542887ad4ab926dead743c7fa4edfd9a4
BadKosmos <thefest188@mail.ru>
2023-05-30 17:32:17 +0300

    123

--- main.py
+++ main.py
@@ -1,59 +1,32 @@
-from random import randint
-
-
 def proc1():
-    list = []
-    for i in range(0, 5):
-        list.append(randint(1, 99))
-
-    inp = int(input())
-    flag: bool = False
-    for i in range(0, 5):
-        print(list[i], end=" ")
-        if list[i] == inp:
-            flag = True
-    print("User numb:", inp)
-    if flag:
-        print("Congratulation, this number stored in list")
+    list = {"USA": "New york", "UK": "Great britan", "Russia": "Moscow", "China": "Chin-Chong",
+            "Australia": "Canberra"}
+    for i in list.keys():
+        print(i, ":", list[i])
+    inp = input()
+    if list.get(inp, -1) == -1:
+        print("This item does not exist")
     else:
-        print("This numb does not exist")
+        print(list[inp])
+    outp = sorted(list.keys())
+    for i in outp:
+        print(i, ": ", list[i])
 
 
 def proc2():
-    list = []
-    for i in range(0, 5):
-        list.append(randint(1, 99))
-    cou = {}
-    for i in range(0, 5):
-        if cou.get(list[i], -1) == -1:
-            cou[list[i]] = 0
-        cou[list[i]] += 1
-
-    for i in cou.keys():
-        print(i, ": ", cou[i])
+    list = {1: "aeioulnstr", 2: "dg", 3: "bcmp", 4: "fhvwy", 5: "k", 8: "jx", 10: "qz"}
+    inp = input()
+    inp = inp.casefold()
+    sum = 0
+    for i in inp:
+        for g in list.keys():
+            if (list[g].find(i) != -1):
+                sum += g
+    print(sum)
 
 
 def proc3():
-    days = ("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
-    print("Amount of day offs:", end="")
-    numb = int(input())
-    print("Day off: ", end=" ")
-    for i in range(7 - numb, 7):
-        print(days[i], end=" ")
-    print()
-    print("Working days: ", end=" ")
-    for i in range(0, 7 - numb):
-        print(days[i], end=" ")
-    print()
+    stud = {"Stud1": "russian,english,french", "Stud2": "chineese,russian,french", "Stud3": ""}
 
 
-def proc4():
-    list1 = ["List1Name1", "List1Name2", "List1Name3", "List1Name4"]
-    list2 = ["List2Name1", "List2Name2", "List2Name3", "List2Name4"]
-    Team = (list1[0], list1[1], list2[2], list2[3])
-    print("Group1:", *list1, sep=" ")
-    print("Group2:", *list2, sep=" ")
-    print("Team List:", *Team, sep=", ")
-    print("TeamSize", len(Team))
-    print("Am of Ivanov", Team.count("Ivanov"))
-
+proc3()
